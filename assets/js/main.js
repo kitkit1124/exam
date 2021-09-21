4e778c970a2323821e06b330e4ffac5f6861383e
kitkit1124 <bunielkeithmark>
2021-09-21 21:46:59 +0800

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("hamburgerMenu").style.width = "100%";
+  }
+  
+function closeNav() {
+    document.getElementById("hamburgerMenu").style.width = "0%";
+}
